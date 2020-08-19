568fa5ffc868feefedf605c971883728ae468ce3
syj2908 <2512849424@qq.com>
2020-08-19 12:37:41 +0800

    code test completed

--- auto_healthreport/auto_healthreport.py
+++ auto_healthreport/auto_healthreport.py
@@ -9,10 +9,10 @@ def auto_healthreport(name, passwd):
     driver.get('http://one.xjtu.edu.cn/EIP/nonlogin/user/index.htm')
     #最大化窗口
     driver.maximize_window()
-    time.sleep(2)
+    time.sleep(3)
     #找到登录按钮
     driver.find_element_by_xpath("//a[@class='login-btn']").click()
-    time.sleep(2)
+    time.sleep(3)
 
     #输入用户名和密码
     driver.find_element_by_xpath("//input[@type='text']").click()
@@ -20,11 +20,11 @@ def auto_healthreport(name, passwd):
     driver.find_element_by_xpath("//input[@type='password']").click()
     driver.find_element_by_xpath("//input[@type='password']").send_keys(passwd)
     driver.find_element_by_xpath("//div[@class='login_btn account_login']").click()
-    time.sleep(2)
+    time.sleep(3)
 
     #登陆成功，跳转到综合服务大厅
     driver.get('http://jkrb.xjtu.edu.cn/EIP/user/index.htm')
-    time.sleep(5)
+    time.sleep(3)
     #有坑，网站嵌套了iframe，需要一层层进入iframe，最终才能点击
     iframelist = []
     iframe = driver.find_element_by_xpath("//*[@id='mini-17$body$2']/iframe")
@@ -35,7 +35,7 @@ def auto_healthreport(name, passwd):
     driver.switch_to.frame(iframelist[1])
     #点击 ‘本科生每日健康状况填报’
     driver.find_element_by_xpath("//*[@id='form']/div[2]/div/ul[1]/li[2]/div").click()
-    time.sleep(2)
+    time.sleep(3)
     #焦点切换回默认frame
     driver.switch_to.default_content()
     iframe = driver.find_element_by_xpath("//*[@id='mini-17$body$3']/iframe")
@@ -43,11 +43,11 @@ def auto_healthreport(name, passwd):
     driver.switch_to.frame(iframelist[2])
     #点击‘返校登记后每日健康填报’
     driver.find_element_by_xpath("/html/body/div/div[2]/div[2]/ul/li[2]/span[1]").click()
-    time.sleep(2)
+    time.sleep(3)
 
     #开始填写表单 焦点切换回默认frame
     driver.switch_to.default_content()
-    iframe = driver.find_element_by_xpath("//*[@id='mini-17$body$6']/iframe")
+    iframe = driver.find_element_by_xpath("//*[@id='mini-17$body$4']/iframe")
     iframelist.append(iframe)
     driver.switch_to.frame(iframelist[3])
     iframe = driver.find_element_by_xpath("//*[@id='mini-14$body$2']/iframe")
@@ -58,12 +58,14 @@ def auto_healthreport(name, passwd):
     #填写今日体温
     temperature = 36 + random.randint(5, 7) * 0.1
     #随机生成36.5~36.7之间的体温
-    driver.find_element_by_xpath("//*[@id='BRTW$text']").send_keys(temperature)
+    driver.find_element_by_xpath("//*[@id='BRTW$text']").send_keys(str(temperature))
 
     #填写完毕 提交按钮在第三层iframe上，我们现在在第四层iframe，因此回到这一层的父层
     driver.switch_to.parent_frame()
     #点击提交按钮
     driver.find_element_by_xpath("//*[@id='sendBtn']").click()
+    driver.find_element_by_xpath("//*[@id='mini-17']/span").click()
+
     time.sleep(5)
 
 if __name__ == '__main__':
